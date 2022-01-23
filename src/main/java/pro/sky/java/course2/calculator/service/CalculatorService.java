03fa2f2b7839f6e495400474d0b4f84e46a8f8f0
pro100 <pro100tkach@mail.ru>
2022-01-23 14:51:16 +0300

    Is done Fix1

--- src/main/java/pro/sky/java/course2/calculator/service/CalculatorService.java
+++ src/main/java/pro/sky/java/course2/calculator/service/CalculatorService.java
@@ -1,10 +1,9 @@
 package pro.sky.java.course2.calculator.service;
 
 public interface CalculatorService {
-    String getCalculatorHello();
 
-    int getCalculatorPlus(int num1, int num2);
-    int getCalculatorMinus(int num1, int num2);
-    int getCalculatorMultiply(int num1, int num2);
-    int getCalculatorDivide(int num1, int num2);
+    int Plus(int num1, int num2);
+    int Minus(int num1, int num2);
+    int Multiply(int num1, int num2);
+    int Divide(int num1, int num2);
 }
